746c1bf41c23d7de25b3e5a14187dbea465e3c9b
Piyush Agrawal <piyushagrawal@Piyushs-MacBook-Air.local>
2025-02-08 13:43:35 +0530

    UI enchancement

--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,45 +1,50 @@
-import { Component, resource, signal } from '@angular/core';
+import { Component, OnInit, resource, signal } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { api } from '../../environment';
 @Component({
   selector: 'app-resource-demo',
-  imports: [MatProgressSpinnerModule, MatListModule],
+  imports: [MatProgressSpinnerModule, MatListModule, CommonModule],
   templateUrl: './resource-demo.component.html',
   styleUrl: './resource-demo.component.css',
 })
-export class ResourceDemoComponent {
-  search = signal<string>('');
+export class ResourceDemoComponent implements OnInit {
+  search = signal<number>(0);
 
-  //resourse requires two generic thing first type of data, resoures api going to fetch and resutn and search parameter,
-  //second it required request and loader,
-  // in request it take search parameter , whenever search signal value change resourese api request function will get latest value and
-  // loader function is the one who going to invoke for fetch data from server so it should be async function
-  // loader function get input argument , so here we receive request
-  users = resource<User[], { search: string }>({
+  users = resource<User[], { search: number }>({
     request: () => ({
       search: this.search(),
     }),
-    
+
     loader: async ({ request, abortSignal }) => {
-      if (request.search) {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users', {
-            signal:abortSignal
-          }
-        );
+      const url = request.search ? `${api}/${request.search}` : api;
+
+      const response = await fetch(url, {
+        signal: abortSignal,
+      });
+    
+      if(response.ok){
         const json = response.json();
-        return json;
+        const data = await json;
+        return Array.isArray(data)? data: [data]
+      } else {
+        throw new Error(`API Error: ${response.status} - ${response.statusText}`);
       }
-      return [];
     },
+    
   });
 
-  setSerachValue(event: string): void {
+  ngOnInit(): void {
+   
+  }
+
+  setSerachValue(event: number): void {
     this.search.set(event);
   }
 
   resetSearch(): void {
-    this.search.set('');
+    this.search.set(0);
   }
 }
 
